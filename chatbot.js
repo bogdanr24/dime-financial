238b93bd1ceaddb7dec826d93be07bfa8a5dc339
ml3787 <ml3787@tux5.cci.drexel.edu>
2024-03-03 17:44:53 -0500

    test

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,18 @@
 function insertText(text) {
-    var inputBox = document.getElementById("inputBox");
+    var inputBox = document.getElementById("prompt");
     inputBox.value = text;
 }
+
+function submitOnEnter(event) {
+    if (event.which === 13 && !event.shiftKey) {
+        if (!event.repeat) {
+            const newEvent = new Event("submit", {cancelable: true})
+            event.target.form.dispatchEvent(newEvent);
+            event.target.value ="";
+        }
+
+        event.preventDefault();
+    }
+}
+
+document.getElementById("prompt").addEventListener("keydown", submitOnEnter);
