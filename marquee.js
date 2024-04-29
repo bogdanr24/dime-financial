c80d9b3abda4049bccebe30b8156a463c1100d39
Aleksei Nikitin <an944@drexel.edu>
2024-04-29 17:17:59 -0400

    put the function that bogdan created to get the marquee moving

--- marquee.js
+++ marquee.js
@@ -45,5 +45,11 @@ function fetchData() {
     });
 }
 
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
 fetchData(); // Call the function
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
